a7953153640d6fa7ceb5597dd2073fc7351ec36d
bhandarr <bhandarr@myumanitoba.ca>
2016-02-21 01:06:32 -0600

    updating MainActivity

--- app/src/main/java/com/example/kushal/Handy/MainActivity.java
+++ app/src/main/java/com/example/kushal/Handy/MainActivity.java
@@ -9,16 +9,19 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-       // import android.support.design.widget.FloatingActionButton;
-        //import android.support.design.widget.Snackbar;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.View;
-        import android.view.Menu;
-        import android.view.MenuItem;
+// import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,7 +37,9 @@ public class MainActivity extends Activity {
     Button newNote;
     TextNotePL textNotePL = new TextNotePL();
     int alSize = 0;// arraylist size
-   // Note noteObj1=new Note();
+
+    ArrayAdapter<String> arrayAdapter;
+    // Note noteObj1=new Note();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +68,9 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), NoteTaker.class);
                 startActivityForResult(intent, 0);
+                dataAdded();
+
+
 
             }
         });
@@ -71,15 +79,16 @@ public class MainActivity extends Activity {
     }
 
 
-    private void populateListView() {
-         listView = (ListView) findViewById(R.id.list);
+    private void populateListView()
+    {
+        listView = (ListView) findViewById(R.id.list);
 
         Note note1=new Note("Rahul","Bhandari","firstNote");
 //
-       textNotePL.setNoteList(note1);
+        textNotePL.setNoteList(note1);
 
         alSize = textNotePL.getNoteList().size();
-       String noteName[] = new String[alSize];
+        String noteName[] = new String[alSize];
 
         for (int i = 0; i < alSize; i++) {
 
@@ -91,19 +100,46 @@ public class MainActivity extends Activity {
         noteList.addAll(Arrays.asList(noteName) );
 
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+        arrayAdapter = new ArrayAdapter<String>
                 (this, R.layout.view_note_data , R.id.textView_notename,noteList );
 
 
         listView.setAdapter(arrayAdapter);
 
 
-        //listView.setOnItemClickListener();
-    }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+            }
+        });
 
 
 
     }
+
+    private void dataAdded()
+    {
+        EditText nameET=(EditText)findViewById(R.id.editText_name);
+        String name=nameET.getText().toString();
+
+        if(!name.isEmpty() && name.length()>0)
+        {
+            arrayAdapter.add(name);
+
+            arrayAdapter.notifyDataSetChanged();
+
+            Toast.makeText(getApplicationContext(), "Note Added" + name, Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+}
+
+
+
+
 //@Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
