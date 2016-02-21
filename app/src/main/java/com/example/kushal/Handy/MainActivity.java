c20fbbd744b3fe6acf468cd11a062e748d5ae2ef
bhandarr <rish.bhandari@gmail.com>
2016-02-20 22:06:57 -0600

    Populating ListView using ArrayList data

--- app/src/main/java/com/example/kushal/Handy/MainActivity.java
+++ app/src/main/java/com/example/kushal/Handy/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.kushal.Handy;
 
 
 import com.example.kushal.rihabhbhandari.R;
+
+import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
-        import android.os.Bundle;
+import android.database.Cursor;
+import android.os.Bundle;
        // import android.support.design.widget.FloatingActionButton;
         //import android.support.design.widget.Snackbar;
         import android.support.v7.app.AppCompatActivity;
@@ -12,14 +16,34 @@ import android.content.Intent;
         import android.view.View;
         import android.view.Menu;
         import android.view.MenuItem;
-        import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import PersistanceLayer.TextNotePL;
+
+import static android.R.layout.simple_list_item_1;
+import static com.example.kushal.rihabhbhandari.R.layout.activity_main;
+
+public class MainActivity extends Activity {
+    ListView listView;
     Button newNote;
+    TextNotePL textNotePL = new TextNotePL();
+    int alSize = 0;// arraylist size
+   // Note noteObj1=new Note();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        populateListView();
+        //textNotePL.getNoteList().add()
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
@@ -33,21 +57,54 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        newNote=(Button)findViewById(R.id.button_take_note);
+        newNote = (Button) findViewById(R.id.button_take_note);
         newNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(v.getContext(),NoteTaker.class);
-                startActivityForResult(intent,0);
+                Intent intent = new Intent(v.getContext(), NoteTaker.class);
+                startActivityForResult(intent, 0);
 
             }
         });
 
 
-
     }
 
-    //@Override
+
+    private void populateListView() {
+         listView = (ListView) findViewById(R.id.list);
+
+        Note note1=new Note("Rahul","Bhandari","firstNote");
+//
+       textNotePL.setNoteList(note1);
+
+        alSize = textNotePL.getNoteList().size();
+       String noteName[] = new String[alSize];
+
+        for (int i = 0; i < alSize; i++) {
+
+            noteName[i] = textNotePL.getNoteList().get(i).noteName.toString();
+
+        }
+
+        ArrayList<String> noteList = new ArrayList<String>();
+        noteList.addAll(Arrays.asList(noteName) );
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, R.layout.view_note_data , R.id.textView_notename,noteList );
+
+
+        listView.setAdapter(arrayAdapter);
+
+
+        //listView.setOnItemClickListener();
+    }
+
+
+
+    }
+//@Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -68,4 +125,3 @@ public class MainActivity extends AppCompatActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-}
