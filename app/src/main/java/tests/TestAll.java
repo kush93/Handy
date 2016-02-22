114d8b4fdd55043e323e540d430541d51ff33ba8
kushal <kushalsharma51@gmail.com>
2016-02-21 20:21:49 -0600

    Kushal and rahul Better test conditions

---
+++ app/src/main/java/tests/TestAll.java
@@ -0,0 +1,24 @@
+package tests;
+import junit.framework.*;
+
+import BusinessLayer.TextNoteBL;
+import PersistanceLayer.TextNotePL;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by kushal on 2016-02-21.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+public class TestAll extends TestSuite
+{
+    public static Test suite()
+    {
+        return  new TestSuiteBuilder(TestAll.class).includeAllPackagesUnderHere().build();
+    }
+    public TestAll()
+    {
+        super();
+    }
+}
