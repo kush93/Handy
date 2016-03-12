b6b2de78304f7891a791c55f6632b2f14054b7fe
bhandarr <rish.bhandari@gmail.com>
2016-03-11 20:37:09 -0600

    Updated TextNoteBL class to access DataBaseHelper class

--- app/src/main/java/businesslayer/TextNoteBL.java
+++ app/src/main/java/businesslayer/TextNoteBL.java
@@ -1,23 +1,27 @@
 package businesslayer;
 
-import persistancelayer.TextNotePL;
+import android.content.Context;
 
+import persistancelayer.DataBaseHelper;
+import persistancelayer.TextNotePL;
 /**
  * Created by rishabhbhandari on 2016-02-18.
  */
 public class TextNoteBL
 {
 
+    DataBaseHelper dataBaseHelper = null;
     TextNotePL textNoteObj=new TextNotePL();
-    // public ArrayList<TextNotePL> noteList=new ArrayList<TextNotePL>();
+    public TextNoteBL(Context context){
+        dataBaseHelper = new DataBaseHelper(context);
+    }
+    public ArrayList<TextNotePL> noteList=new ArrayList<TextNotePL>();
 
-
-
-
-
-    public void create(String  noteName, String noteLabel, String note)
+    public void create(String  noteName, String noteLabel, String note) // accesses the database as well as arraylist
     {
         //textNoteObj= new TextNotePL(noteName,noteLabel,note);
+        String time = "12:00"; //for the time being, time is hard coded & text note doesn't have any path as it is not saved on the memory
+        dataBaseHelper.insertData(time,noteName , noteLabel, note,null);
         textNoteObj.addData(noteName,noteLabel,note);
         //System.out.printf("11111 %s %s %s ",notename,notelabel,noteit);
     }
