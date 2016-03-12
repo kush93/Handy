c5ae8e382de00714f2fd3bd28b785b2f43958aba
Matthias <iMatthias@users.noreply.github.com>
2016-03-12 08:39:51 -0600

    DataInterface: Updated DataInterface for more requirements of Listview

--- app/src/main/java/persistancelayer/DataInterface.java
+++ app/src/main/java/persistancelayer/DataInterface.java
@@ -1,12 +1,29 @@
 package persistancelayer;
 
+import android.graphics.Bitmap;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by rishabhbhandari on 2016-02-20.
+ *
+ * Updated by Matthias on 2016-03-12 for more methods required by main listview.
  */
 public interface DataInterface {
 
 
-    public void addData(String noteName, String labelName,String note);
+    void addData(String noteName, String labelName,String note);
+
+    // required for main listview
+    boolean hasThumbnail();         // ic_pencil, ic_drawing if null, first image in PhotoNote, or all / some drawing in Handwriting
+    boolean isPinned();             // pin / favourite
+    boolean hasTag();
+    Bitmap getThumbnail();
+    String getTitle();
+    String getContents();
+    List<String> getTags();
+    String getLastEditedTime();
+
+
 }
