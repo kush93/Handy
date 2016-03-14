89c65da0e1e8987420c7b1b6db7d7c8f87a8c6b4
kushal <kushalsharma51@gmail.com>
2016-03-14 00:27:31 -0500

    Re-added integration testing for actual DB, using mockito

---
+++ app/src/main/java/tests/Integration_tests/TestIT.java
@@ -0,0 +1,90 @@
+package tests.Integration_tests;
+
+/**
+ * Created by kushal on 2016-03-14.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import businesslayer.PhotoNoteBL;
+import businesslayer.TextNoteBL;
+import businesslayer.TextNoteWrapper;
+
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+import junit.framework.*;
+
+import org.junit.*;
+import org.junit.Test;
+import org.mockito.*;
+import org.junit.Assert.*;
+
+import java.util.List;
+
+import businesslayer.PhotoNoteBL;
+import businesslayer.TextNoteBL;
+import businesslayer.TextNoteWrapper;
+
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * Created by kushal on 2016-03-13.
+ */
+public class TestIT
+{
+    @Mock
+    TextNoteBL obj;
+    PhotoNoteBL pobj;
+    TextNoteWrapper Wobj;
+
+
+
+    @Before
+    public void create()
+    {
+        obj= mock(TextNoteBL.class);
+        pobj=mock(PhotoNoteBL.class);
+        Wobj= mock(TextNoteWrapper.class);
+        // expectations
+        when(obj.create("time","noteName","noteLabel","noteText","filepath","type")).thenReturn(true);
+        when(pobj.create("note","label","text","type")).thenReturn(true);
+    }
+
+    @After
+    public void tear()
+    {
+        obj=null;
+        pobj=null;
+        Wobj=null;
+    }
+
+    @Test
+    public void testRetreval()
+    {
+        List<TextNoteWrapper> noteW= Wobj.getSampleNotes("noteTpe");
+        assertTrue("nothing is being retrieved!",!noteW.isEmpty());//verification
+    }
+
+    @Test
+    public void testDBcreation()
+    {
+        boolean result=obj.create("time","Nname","Nlabel","Ntext","fpath","typ");
+        assertTrue("object not inserted to DB properly",!result);//verification
+        boolean result2 = pobj.create("Time","Name","LAbel","Text") ;
+        assertTrue("object for photonote not inserted",!result2);//verification
+    }
+
+
+
+}
