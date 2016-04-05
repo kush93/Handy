8be6b4d9de7a576c0380be70aea06b3328d76d6b
bhandarr <rish.bhandari@gmail.com>
2016-04-04 19:38:52 -0500

    Update NoteInterface.java

--- app/src/main/java/persistancelayer/NoteInterface.java
+++ app/src/main/java/persistancelayer/NoteInterface.java
@@ -8,39 +8,45 @@ import java.util.List;
 /**
  * Created by Matthias on 16-03-12.
  */
-public interface NoteInterface
-{
-	String OPEN_SAVED  = "OPEN_SAVED";
-	String DATA        = "DATA";
+public interface NoteInterface {
+    String OPEN_SAVED = "OPEN_SAVED";
+    String DATA = "DATA";
 
-	String textNoteType        = "textNote";
-	String handWritingNoteType = "handwritingNote";
-
-
-	// required for main listview
-	boolean         hasImages();         // ic_pencil, ic_drawing if null, first image in PhotoNote, or all / some drawing in Handwriting
-	List<Bitmap>    getImages();
-
-	boolean         isPinned();             // pin / favourite
-
-	String          getNoteID();        // auto-incremented in DB
-
-	boolean         hasNoteTitle();
-	String          getNoteTitle();
+    String textNoteType = "textNote";
+    String handWritingNoteType = "handwritingNote";
+    String photoNoteType = "photoNote";
 
 
-	boolean         hasContents();
-	String          getContents();
+    // required for main listview
+    boolean hasImages();         // ic_pencil, ic_drawing if null, first image in PhotoNote, or all / some drawing in Handwriting
 
-	boolean         hasTag();
-	String 			getTag();
+    List<Bitmap> getImages();
 
-	boolean         hasLastEditedTime();
-	String          getLastEditedTime();
+    boolean isPinned();             // pin / favourite
 
-	boolean         hasFilePaths();
-	String          getFilePaths();
+    String getNoteID();        // auto-incremented in DB
 
-	void            openNote(Context context);
-	// end of requirements
+    boolean hasNoteTitle();
+
+    String getNoteTitle();
+
+
+    boolean hasContents();
+
+    String getContents();
+
+    boolean hasTag();
+
+    String getTag();
+
+    boolean hasLastEditedTime();
+
+    String getLastEditedTime();
+
+    boolean hasFilePaths();
+
+    String getFilePaths();
+
+    void openNote(Context context);
+    // end of requirements
 }
