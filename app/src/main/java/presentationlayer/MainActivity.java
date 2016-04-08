9aa07d96006c01f06c756e3b2b27e8d2c1612c7a
rbhandari07 <bhandar3@myumanitoba.ca>
2016-04-08 12:36:10 -0500

    DND error correction

--- app/src/main/java/presentationlayer/MainActivity.java
+++ app/src/main/java/presentationlayer/MainActivity.java
@@ -126,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
